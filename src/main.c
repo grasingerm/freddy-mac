cd00bcf62aa8e09a89dd04b304c0901e1ce80a37
Matthew Grasinger <grasingerm@gmail.com>
2016-04-07 15:55:36 -0400

    fetch instruction and evaluate loop

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+
 typedef enum {
   PSH,
   ADD,
@@ -5,3 +7,34 @@ typedef enum {
   SET,
   HLT
 } InstructionSet;
+
+const int program[] = {
+  PSH, 5,
+  PSH, 6,
+  ADD,
+  POP,
+  HLT
+};
+
+bool running = true;
+int ip = 0;
+
+int fetch() {
+  return program[ip];
+}
+
+void eval(int instr) {
+  switch (instr) {
+    case HLT:
+      running = false;
+      break;
+  }
+}
+
+int main() {
+  while (running) {
+    eval(fetch());
+    ip++;
+  }
+  return 0;
+}
